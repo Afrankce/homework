8db7de90057af76c60e69cebdcea85fdbdbe00c2
Afrankce <80972479+Afrankce@users.noreply.github.com>
2024-10-03 16:19:48 +0800

    Add files via upload

---
+++ cfiles/main.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{ int m,n,i,j,target;
+printf("please input m and n");
+scanf("%d",&m);
+scanf("%d",&n);  
+int a[m][n];
+for(i=0;i<m;i++)
+  for(j=0;j<n;j++)
+scanf("%d",&a[i][j]);
+printf("Please input target");
+scanf("%d",&target);
+for(i=0;i<m;i++)
+{
+  for(j=0;j<n;j++)
+  if(a[i][j]==target)
+  {printf("true"); 
+  return 0;
+  }
+}
+if (i==m)
+printf("flase");
+}
